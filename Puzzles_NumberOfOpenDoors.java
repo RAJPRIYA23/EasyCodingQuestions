401c2a6ebda30496948ff529d08839fe4a35841b
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-22 15:39:33 +0530

    Create Puzzles_NumberOfOpenDoors.java

---
+++ Puzzles_NumberOfOpenDoors.java
@@ -0,0 +1,45 @@
+/*
+Consider a long alley with a N number of doors on one side. All the doors are closed initially. 
+You move to and fro in the alley changing the states of the doors as follows: you open a door that is already closed and you close a door that is already opened. 
+You start at one end go on altering the state of the doors till you reach the other end and then you come back and start altering the states of the doors again.
+In the first go, you alter the states of doors numbered 1, 2, 3, … , n.
+In the second go, you alter the states of doors numbered 2, 4, 6…
+In the third go, you alter the states of doors numbered 3, 6, 9 …
+You continue this till the Nth go in which you alter the state of the door numbered N.
+You have to find the number of open doors at the end of the procedure.
+
+Example:
+
+Input:
+N = 2
+Output:
+1
+
+Explanation:
+Initially all doors are closed.
+After 1st go, all doors will be opened.
+After 2nd go second door will be closed.
+So, Only 1st door will remain Open.
+*/
+
+//
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG 
+{
+	public static void main (String[] args) throws Exception
+	{
+		BufferedReader br = new BufferedReader
+		(new InputStreamReader(System.in)); 
+        int t = Integer.parseInt(br.readLine()); 
+        for(int i=1;i<=t;i++)
+        {
+            long n = Long.parseLong(br.readLine()); 
+            System.out.println((long)Math.floor(Math.sqrt(n)));
+        }
+	}
+}
