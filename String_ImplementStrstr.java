1027028e72d0c330dbc0fe3b2696f6bf880abc4e
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-23 19:23:33 +0530

    Create String_ImplementStrstr.java

---
+++ String_ImplementStrstr.java
@@ -0,0 +1,38 @@
+/*
+Your task is to implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
+
+Example 1:
+
+Input:
+s = GeeksForGeeks, x = Fr
+Output: -1
+Explanation: Fr is not present in the
+string GeeksForGeeks as substring.
+
+Example 2:
+Input:
+s = GeeksForGeeks, x = For
+Output: 5
+Explanation: For is present as substring
+in GeeksForGeeks from index 5 (0 based
+indexing).
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Your are required to complete this method 
+
+class GfG
+{
+    int strstr(String str, String target)
+    {           
+        for(int i=0; i< str.length()-target.length()+1; i++)
+        {
+            if(str.substring(i,i+target.length()).equals(target))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
