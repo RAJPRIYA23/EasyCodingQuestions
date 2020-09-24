b77fd2e918e8f2081eb6e8e8dc430ed0a93c9ac7
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-24 22:06:22 +0530

    Create Sorting_InsertionSort.java

---
+++ Sorting_InsertionSort.java
@@ -0,0 +1,85 @@
+/*
+The task is to complete the insert() function which is used to implement Insertion Sort.
+
+Example:
+
+Input: N = 5, arr[] = { 4, 1, 3, 9, 7}
+Output: 1 3 4 7 9
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	static void insertionSort(int arr[],int n)
+		{
+			int i;
+			for(i=1;i<n;i++)
+			new InsertionSort().insert(arr,i);
+		}
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}// } Driver Code Ends
+
+
+/* The task is to complete insert() which is used 
+   as shown below to implement insertionSort() */
+
+/* Function to sort an array using insertion sort
+static void insertionSort(int arr[], int n)
+{
+  GfG obj = new GfG();
+   for (int i = 1; i < n; i++)
+      obj.insert(arr, i);
+} */
+
+class InsertionSort
+{
+    static void insert(int arr[],int i)
+    {
+        int value = arr[i]; 
+        int j= i-1;
+        
+        
+        //shifting
+        for(; j >= 0; j--)
+        {
+            if(arr[j] > value)
+                arr[j+1] = arr[j];
+            else
+                break;
+        }
+
+        //assign
+        arr[j+1] = value;
+
+    }
+}
