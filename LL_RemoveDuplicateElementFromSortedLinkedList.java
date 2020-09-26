ba72b0040392bf05b0af4f77ad8df579a4a7697d
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-26 21:56:42 +0530

    Create LL_RemoveDuplicateElementFromSortedLinkedList.java

---
+++ LL_RemoveDuplicateElementFromSortedLinkedList.java
@@ -0,0 +1,117 @@
+/*
+Given a singly linked list consisting of N nodes. The task is to remove duplicates (nodes with duplicate values) from the given list (if exists).
+
+Example:
+
+Input:
+LinkedList: 2->2->4->5
+Output: 2 4 5
+Explanation: In the given linked list 
+2 ->2 -> 4-> 5, only 2 occurs more 
+than 1 time.
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* package whatever; // don't place package name! */
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class Remove_Duplicate_From_LL
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        GfG g = new GfG();
+		llist.head = g.removeDuplicates(llist.head);
+		llist.printList();
+		
+        t--;		
+        }
+    }}// } Driver Code Ends
+
+
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class GfG
+{
+    // head: head node
+    Node removeDuplicates(Node root)
+    {
+	
+	    Node temp = root, prev = root;
+	    Boolean match = false;
+	    while(temp!=null && temp.next!=null)
+	    {
+	        if(temp.data == temp.next.data && !match)
+            {
+                prev = temp;
+                match = true;
+            }
+            
+            else if(temp.data != temp.next.data)
+            {
+                prev.next = temp.next;
+                prev = temp.next;
+                match =false;
+            }
+            
+	        temp = temp.next;
+	    }
+	    
+	    prev.next = null;
+	    
+	    return root;
+    }
+}
