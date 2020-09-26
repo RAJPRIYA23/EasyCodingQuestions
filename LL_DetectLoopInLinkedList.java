e02732954bcded9e7b58294daedac21f74cc5a84
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-26 22:07:38 +0530

    Create LL_DetectLoopInLinkedList.java

---
+++ LL_DetectLoopInLinkedList.java
@@ -0,0 +1,114 @@
+/*
+Given a linked list of N nodes. The task is to check if the the linked list has a loop. Linked list can contain self loop.
+
+Example:
+
+Input:
+N = 3
+value[] = {1,3,4}
+x = 2
+Output: True
+Explanation: In above test case N = 3.
+The linked list with nodes N = 3 is
+given. Then value of x=2 is given which
+means last node is connected with xth
+node of linked list. Therefore, there
+exists a loop.
+*/
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution 
+{
+    public static boolean detectLoop(Node head)
+    {
+        Node temp = head;
+        
+        while(temp!= null && temp.data > 0)
+        {
+            temp.data = -(temp.data);
+            temp = temp.next;
+        }
+        
+        if(temp == null)
+            return false;
+        return true;
+    }
+}
