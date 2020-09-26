86ec74956e246c9d358d1806fd1d8800d040440c
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-26 17:45:16 +0530

    Create Queue_QueueUsingTwoStacks.java

---
+++ Queue_QueueUsingTwoStacks.java
@@ -0,0 +1,115 @@
+/*
+Implement a Queue using 2 stacks s1 and s2 .
+A Query Q is of 2 Types
+(i) 1 x (a query of this type means  pushing 'x' into the queue)
+(ii) 2   (a query of this type means to pop element from queue and print the poped element)
+
+Example 1:
+
+Input:
+5
+1 2 1 3 2 1 4 2
+
+Output: 
+2 3
+
+Explanation: 
+In the first testcase
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the queue 
+    will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3.
+
+Your Task:
+You are required to complete the two methods push which take one argument an integer 'x' to be pushed into the queue and pop which returns a integer poped out from other queue(-1 if the queue is empty). 
+The printing is done automatically by the driver code.
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+import java.util.Stack;
+import java.util.LinkedList;
+
+class GfG
+{
+	public static void main(String args[])
+	{
+	    //Taking input using class Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Taking input the number of testcases
+		int t = sc.nextInt();
+		while(t>0)
+		{
+		    //Creating a new object of class StackQueue
+			StackQueue g = new StackQueue();
+			
+			//Taking input the total number of Queries
+			int q = sc.nextInt();
+			while(q>0)
+			{
+				int QueryTyoe = sc.nextInt();
+				
+				//If QueryTyoe is 1 then
+				//we call the Push method
+				//of class StackQueue
+				//else we call the Pop method
+				if(QueryTyoe == 1)
+				{
+					int a = sc.nextInt();
+					g.Push(a);
+				}else
+				if(QueryTyoe == 2)
+				System.out.print(g.Pop()+" ");
+			q--;
+				
+			}
+			System.out.println();
+		t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class StackQueue
+{
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    
+    void Push(int x)
+    {
+	   s1.push(x);
+    }
+	
+    
+    int Pop()
+    {
+        if(s1.isEmpty())
+            return -1;
+            
+	    Stack<Integer> tempStack = new Stack<Integer>();
+	    
+	    int res= 0;
+	    
+	    while(s1.size()!=1)
+	    {
+	        s2.push(s1.pop());
+	    }
+	    
+	    res = s1.pop();
+	    
+	    while(!s2.isEmpty())
+	    {
+	        s1.push(s2.pop());
+	    }
+	    
+	    return res;
+    }
+    
+}
