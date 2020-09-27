0156e5099bfd46eaef17d5bad955a0592cdcb77f
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-27 17:11:02 +0530

    Create LL_CircularLinkedListTraversal.c

---
+++ LL_CircularLinkedListTraversal.c
@@ -0,0 +1,97 @@
+/*
+Given a circular linked list, your task is to complete the method printList() that prints the linked list.
+
+Constraints:
+1<=T<=50
+1<=N<=50
+
+Example:
+Input:
+2
+7
+374 363 171 497 282 306 426
+2
+162 231
+Output:
+426 306 282 497 171 363 374
+231 162
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void push(struct Node **head_ref, int data)
+{
+    struct Node *ptr1 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *temp = *head_ref;
+    ptr1->data = data;
+    ptr1->next = *head_ref;
+
+    if (*head_ref != NULL)
+    {
+        while (temp->next != *head_ref)
+            temp = temp->next;
+        temp->next = ptr1;
+    }
+    else
+        ptr1->next = ptr1; 
+
+    *head_ref = ptr1;
+}
+
+void printList(struct Node*);
+
+int main()
+{
+    int i,n,t,val;
+    scanf("%d",&t);
+
+    while(t--)
+        {
+         struct Node *head = NULL;
+         scanf("%d",&n);
+         for(i=0;i<n;i++)
+          {
+            scanf("%d",&val);
+            push(&head,val);
+          }
+         printList(head);
+         printf("\n");
+        }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* structure for a node 
+struct Node
+{
+    int data;
+    struct Node *next;
+}; */
+
+/* Function to print nodes in a given Circular linked list */
+void printList(struct Node *head)
+{
+    struct Node *curr = head;
+    while(curr->data >= 0)
+    {
+        curr->data = -(curr->data)-1;
+        curr = curr -> next;
+    }
+    
+    while(curr->data < 0)
+    {
+        curr->data = -(curr->data)-1;
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+}
