175c93ddbc79bbcb11c099eec49e4aa668315482
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-27 19:59:10 +0530

    Create Tree_LowestCommonAncestorInABinaryTree.java

---
+++ Tree_LowestCommonAncestorInABinaryTree.java
@@ -0,0 +1,167 @@
+/*
+Given a Binary Tree with all unique values and two nodes value n1 and n2. The task is to find the lowest common ancestor of the given two nodes.
+We may assume that either both n1 and n2 are present in the tree or none of them is present. 
+
+Example 1:
+
+Input:
+n1 = 2 , n2 =  3
+
+     1
+   /  \
+  2    3
+Output: 1
+
+Your Task:
+ Just complete the function lca() that takes nodes, n1, and n2 as parameters and returns LCA node as output.
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String input[] = br.readLine().trim().split(" ");
+                int a = Integer.parseInt(input[0]);
+                int b = Integer.parseInt(input[1]);
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                Tree g = new Tree();
+                Node k = g.lca(root,a,b);
+    			System.out.println(k.data);
+    	        
+	        }
+	}
+}
+// } Driver Code Ends
+
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Tree
+{
+
+/* If n1 and n2 are present, return reference
+   to LCA. If both are not present, return 
+   null,. Else if left subtree contains any 
+   of them return pointer to left.*/
+	Node lca(Node root, int n1,int n2)
+	{
+		if(root == null)
+		    return null;
+		
+		if(root.data == n1 || root.data == n2)
+		{
+		    return root;
+		}
+		
+		Node left = lca(root.left, n1, n2);
+		Node right = lca(root.right, n1, n2);
+		
+		if(left != null & right != null)
+		    return root;
+		 
+		return (left != null) ? left : right; 
+	}
+}
+
